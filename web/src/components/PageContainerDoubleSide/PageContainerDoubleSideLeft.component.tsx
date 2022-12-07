1aad35e5f0b3357eb31105b848fefde3bf0b18f1
amrh07@gmail.com <amrh07@gmail.com>
2022-12-07 09:49:43 -0300

    feat(web): Updated double side layout gap value

--- web/src/components/PageContainerDoubleSide/PageContainerDoubleSideLeft.component.tsx
+++ web/src/components/PageContainerDoubleSide/PageContainerDoubleSideLeft.component.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 function PageContainerDoubleSideLeft(props: PropsWithChildren) {
   return (
-    <div className="flex lg:max-w-[415px] flex-col w-full relative">
+    <div className="flex lg:max-w-[419px] flex-col w-full relative">
       {props.children}
     </div>
   );
