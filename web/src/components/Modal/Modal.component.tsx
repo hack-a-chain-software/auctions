d533631a7d991a8fa7ee5f3d470d4598deca6ff9
Alexandre <alexandre01393@gmail.com>
2022-12-06 23:11:54 -0300

    fix(web): fixing modal and using theme predefined

--- web/src/components/Modal/Modal.component.tsx
+++ web/src/components/Modal/Modal.component.tsx
@@ -1,6 +1,5 @@
 import { Dialog } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/solid";
-import { useState } from "react";
 
 type ModalProps = {
   open: boolean;
@@ -15,35 +14,35 @@ function Modal({ open, onClose }: ModalProps) {
       onClose={onClose}
     >
       <Dialog.Backdrop className="fixed inset-0 bg-black/50 z-50" />
-      <Dialog.Panel className="relative bg-white rounded-[22px] shadow-2 overflow-hidden w-[95%] max-w-2xl shadow-[-1px_0px_16px_0px_rgba(0,0,0,0.21)]">
+      <Dialog.Panel className="relative bg-white rounded-lg shadow-md overflow-hidden w-[95%] max-w-[620px] h-[443px]">
         <div className="w-full flex justify-between items-center pl-8 pr-6 h-12 border-b-[1px] ">
-          <Dialog.Title className="text-black px-1 font-semibold tracking-tight">
+          <Dialog.Title className="text-black px-1 text-sm font-semibold tracking-tight">
             Select NFTs
           </Dialog.Title>
-          <button className="w-[22px] h-[22px] text-black">
+          <button className="w-[22px] h-[22px] text-title">
             <XMarkIcon />
           </button>
         </div>
-        <ul className="grid grid-cols-auto-fit p-4 justify-items-center gap-6 max-h-[450px] overflow-y-auto">
-          {[1, 2, 3, 4, 5, 6].map((item) => (
+        <ul className="grid grid-cols-auto-fit p-4 justify-items-center gap-4 max-h-[310px] overflow-y-auto">
+          {[1, 2, 3].map((item) => (
             <li className="relative">
               <img
                 src="https://assets-global.website-files.com/5e73a1e3ba24f2cd5dd2232a/62f66985e6fa143898ba6762_Como%20criar%20um%20NFT%20(1)%20(1).jpg"
                 alt=""
-                className="w-[190px] h-[190px] object-cover rounded-[15px]"
+                className="w-[169px] h-[175px] object-cover rounded-md"
               />
-              <div className="flex justify-around flex-col absolute backdrop-blur-sm bg-white/30 bottom-2 rounded-[15px] w-[95%] left-1">
-                <h3 className="text-black font-bold ml-4">Doodles</h3>
-                <span className="text-black font-bold ml-4">#3333</span>
+              <div className="flex justify-around flex-col absolute backdrop-blur-cover bg-white/30 bottom-2 rounded h-[46px] w-[149px] left-[0.6rem]">
+                <h3 className="text-black font-bold text-sm ml-4">Doodles</h3>
+                <span className="text-black font-bold text-sm ml-4">#3333</span>
               </div>
             </li>
           ))}
         </ul>
-        <footer className="border-t-[1px] px-8 py-4 flex justify-between">
-          <button className="w-[130px] border-solid border-gray-300 border-[1px] rounded-[17px] p-2 text-gray-400 font-semibold">
+        <footer className="absolute w-full bottom-0 border-t-[1px] h-[73px] px-8 py-4 flex justify-between">
+          <button className="w-[120px] border-solid border-outline border-[1px] rounded-md p-2 text-text text-sm font-semibold">
             Cancel
           </button>
-          <button className="w-[130px] bg-[#474EFF] rounded-[17px]">
+          <button className="w-[130px] bg-button rounded-md text-highlight text-sm">
             Add to auction
           </button>
         </footer>
