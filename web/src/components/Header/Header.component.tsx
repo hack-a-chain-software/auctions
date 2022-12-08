877fd29c5c7e5cfacb69ade1bedd2b3d0e0f778e
Alexandre <alexandre01393@gmail.com>
2022-12-08 09:45:58 -0300

    fix(web): fixing header spacing and sizes

--- web/src/components/Header/Header.component.tsx
+++ web/src/components/Header/Header.component.tsx
@@ -1,8 +1,9 @@
 import {
   Bars3Icon,
   XMarkIcon,
-  ChevronRightIcon,
+  ChevronDownIcon,
   MagnifyingGlassIcon,
+  ChevronRightIcon,
 } from "@heroicons/react/24/solid";
 import { WalletIcon } from "@heroicons/react/24/outline";
 import { Fragment, useState } from "react";
@@ -14,21 +15,21 @@ function Header() {
   const [open, setOpen] = useState<boolean>(false);
 
   return (
-    <header className="bg-white flex flex-col justify-between items-center w-full mb-2 sticky top-0 backdrop-blur-sm bg-white/50 xl:px-32">
+    <header className="bg-white flex flex-col justify-between items-center w-full mb-2 md:h-20 sticky top-0 backdrop-blur-sm bg-white/50 xl:px-24 xl:py-1">
       <div className="flex justify-between items-center p-4 w-full">
         <Bars3Icon
           className="text-black w-8 md:hidden"
           onClick={() => setOpen(true)}
         />
-        <div className="hidden md:flex items-center gap-4">
+        <div className="hidden md:flex items-center gap-4 pl-1">
           <SpaceBitLogo38 />
           <h1 className="uppercase text-black tracking-[0.2rem] text-xl font-bold font-['Poppins']">
             Spacebit
           </h1>
         </div>
         <nav className="hidden md:flex">
-          <ul className="flex gap-12 items-center">
-            <li>
+          <ul className="flex gap-[2.7rem] items-center">
+            <li className="pb-1">
               <a
                 href="#"
                 className="text-black font-semibold text-sm tracking-tight"
@@ -36,50 +37,42 @@ function Header() {
                 Explore auctions
               </a>
             </li>
-            <Popover as="li" className="relative">
-              {({ open }) => (
-                <>
-                  <Popover.Button className="text-black font-semibold text-sm flex items-center gap-2 tracking-tight outline-none">
-                    My auctions
-                    <ChevronRightIcon
-                      className={`text-black w-4 ${
-                        open ? "rotate-90 transform" : ""
-                      }`}
-                    />
-                  </Popover.Button>
+            <Popover as="li" className="relative pr-8">
+              <Popover.Button className="text-black font-semibold text-sm flex items-center gap-2 tracking-tight outline-none">
+                My auctions
+                <ChevronDownIcon className="text-black w-4" />
+              </Popover.Button>
 
-                  <Transition
-                    enter="transition duration-100 ease-out"
-                    enterFrom="transform scale-95 opacity-0"
-                    enterTo="transform scale-100 opacity-100"
-                    leave="transition duration-75 ease-out"
-                    leaveFrom="transform scale-100 opacity-100"
-                    leaveTo="transform scale-95 opacity-0"
+              <Transition
+                enter="transition duration-100 ease-out"
+                enterFrom="transform scale-95 opacity-0"
+                enterTo="transform scale-100 opacity-100"
+                leave="transition duration-75 ease-out"
+                leaveFrom="transform scale-100 opacity-100"
+                leaveTo="transform scale-95 opacity-0"
+              >
+                <Popover.Panel className="flex flex-col bg-white items-start justify-center w-[179px] h-[141px] rounded-lg shadow p-4 absolute top-2 right-0">
+                  <a
+                    href="#"
+                    className="text-black font-semibold text-sm w-full p-3 border-b-[1px] tracking-tight"
                   >
-                    <Popover.Panel className="flex flex-col bg-white items-start justify-center w-[179px] h-[141px] rounded-lg shadow-[0px_4px_20px_rgba(0,0,0,0.1)] p-4 absolute top-2 right-0">
-                      <a
-                        href="#"
-                        className="text-black font-semibold text-sm w-full p-3 border-b-[1px] tracking-tight"
-                      >
-                        My offers
-                      </a>
-                      <a
-                        href="#"
-                        className="text-black font-semibold text-sm w-full p-3 tracking-tight"
-                      >
-                        Create auction
-                      </a>
-                    </Popover.Panel>
-                  </Transition>
-                </>
-              )}
+                    My offers
+                  </a>
+                  <a
+                    href="#"
+                    className="text-black font-semibold text-sm w-full p-3 tracking-tight"
+                  >
+                    Create auction
+                  </a>
+                </Popover.Panel>
+              </Transition>
             </Popover>
-            <li className="ml-10">
+            <li className="pr-7">
               <button
                 type="button"
-                className="bg-space flex justify-center items-center w-[150px] h-[40px] gap-2 text-sm font-bold rounded-md text-white"
+                className="bg-space flex justify-center items-center w-[150px] h-10 gap-2 text-sm font-bold rounded-md text-white tracking-tight"
               >
-                <WalletIcon className="w-5 space-fill-white" /> Connect wallet
+                <WalletIcon className="w-3 text-caption" /> Connect wallet
               </button>
             </li>
           </ul>
@@ -113,11 +106,11 @@ function Header() {
                     <Dialog.Panel
                       className="
                       w-[257px]
-                      shadow-[0px_4px_15px_rgba(151,71,255,0.2)]
+                      shadow-bar
                       pointer-events-auto
-                      relative w-60 h-[100vh] bg-white rounded-tr-lg rounded-br-lg  "
+                      relative w-60 h-screen bg-white rounded-tr-lg rounded-br-lg"
                     >
-                      <div className="w-full flex p-4 items-center justify-between">
+                      <div className="w-full h-[55px] flex p-4 px-5 items-center justify-between">
                         <button
                           type="button"
                           className="text-black hover:text-white focus:outline-none"
@@ -131,8 +124,8 @@ function Header() {
                         <SpaceBitLogo27 />
                       </div>
                       <nav>
-                        <ul className="flex flex-col gap-4 justify-center">
-                          <li className="h-[30px] border-b-[1px] w-full px-6">
+                        <ul className="flex flex-col gap-3 mt-4 justify-center tracking-tight">
+                          <li className="h-[35px] border-b-[2px] w-full px-6">
                             <a
                               href=""
                               className="text-black font-semibold text-sm"
@@ -142,14 +135,14 @@ function Header() {
                           </li>
                           <Popover
                             as="li"
-                            className="h-[30px] border-b-[1px] w-full px-6"
+                            className="h-[33px] border-b-[2px] w-full px-6"
                           >
                             {({ open }) => (
                               <>
-                                <Popover.Button className="text-black font-semibold text-sm flex items-center gap-4 outline-none">
+                                <Popover.Button className="text-black font-semibold text-sm flex items-center gap-4 outline-none  tracking-tight">
                                   My auctions
                                   <ChevronRightIcon
-                                    className={`text-black w-4 ${
+                                    className={`text-black w-[13px] ${
                                       open ? "rotate-90 transform" : ""
                                     }`}
                                   />
@@ -162,16 +155,16 @@ function Header() {
                                   leaveFrom="transform scale-100 opacity-100"
                                   leaveTo="transform scale-95 opacity-0"
                                 >
-                                  <Popover.Panel className="flex flex-col gap-1 mt-4">
+                                  <Popover.Panel className="flex flex-col gap-2 mt-7">
                                     <a
                                       href="#"
-                                      className="text-black font-semibold text-sm "
+                                      className="text-black font-semibold text-sm trancking-tight outline-none"
                                     >
                                       My offers
                                     </a>
                                     <a
                                       href="#"
-                                      className="text-black font-semibold text-sm "
+                                      className="text-black font-semibold text-sm trancking-tight outline-none"
                                     >
                                       Create auction
                                     </a>
@@ -190,7 +183,7 @@ function Header() {
           </Dialog>
         </Transition.Root>
         <nav className="md:hidden">
-          <ul className="flex items-center gap-6">
+          <ul className="flex items-center gap-[3.3rem]">
             <li>
               <a href="#" className="text-black text-sm font-semibold">
                 Explore auctions
@@ -199,20 +192,20 @@ function Header() {
             <li>
               <button
                 type="button"
-                className="bg-space flex items-center w-[150px] h-[32px] justify-center gap-2 text-sm font-bold rounded text-white  "
+                className="bg-space flex items-center w-[150px] h-8 justify-center gap-2 text-sm font-bold rounded text-white"
               >
-                <WalletIcon className="w-5" /> Connect wallet
+                <WalletIcon className="w-5 text-caption" /> Connect wallet
               </button>
             </li>
           </ul>
         </nav>
       </div>
-      <div className="flex gap-4 justify-between w-[95%] max-w-[1224px] h-[40px] bg-input rounded-sm mt-6 mb-6 md:hidden">
-        <MagnifyingGlassIcon className="text-gray-400 w-6 ml-4" />
+      <div className="flex gap-4 justify-between w-[95%] max-w-[1224px] h-10 bg-input rounded-sm mt-6 mb-6 md:hidden">
+        <MagnifyingGlassIcon className="text-paragraph w-6 ml-4" />
         <input
           type="search"
           placeholder="Search for an auction"
-          className="bg-transparent w-full text-black outline-none text-sm"
+          className="bg-transparent w-full text-black outline-none text-sm placeholder-text font-semibold"
         />
       </div>
     </header>
