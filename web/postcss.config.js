3e86889a8039233e0036faab8181fd51f6db698c
Lara Vieira <contato@laravieira.me>
2022-12-07 21:21:01 -0300

    fix(web): Added less loader.

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -3,4 +3,26 @@ module.exports = {
     tailwindcss: {},
     autoprefixer: {},
   },
+  rules: [
+    {
+      test: /\.less$/i,
+      use: [
+        {
+          loader: "style-loader",
+        },
+        {
+          loader: "css-loader",
+        },
+        {
+          loader: "less-loader",
+          options: {
+            lessOptions: {
+              strictMath: true,
+              javascriptEnabled: true
+            },
+          },
+        },
+      ],
+    }
+  ]
 };
