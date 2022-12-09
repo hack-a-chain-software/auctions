208ce7113631febeb803ec00e4d29040372bcc34
Alexandre <alexandre01393@gmail.com>
2022-12-08 23:15:51 -0300

    feat(web): create auction page

---
+++ web/src/assets/svg/CheckedIcon.tsx
@@ -0,0 +1,20 @@
+function CheckedIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="none"
+      viewBox="0 0 16 16"
+    >
+      <path
+        fill="#24B817"
+        fillRule="evenodd"
+        d="M0 8a8 8 0 1116 0A8 8 0 010 8zm10.962-1.488a.615.615 0 00-1.001-.716L7.306 9.513 5.974 8.18a.615.615 0 10-.87.87l1.845 1.847a.615.615 0 00.936-.078l3.077-4.307z"
+        clipRule="evenodd"
+      ></path>
+    </svg>
+  );
+}
+
+export default CheckedIcon;
