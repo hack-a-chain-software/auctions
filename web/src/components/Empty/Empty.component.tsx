3679f3fb1ac2ce6e4993b949dc61fe8ae7a70d3c
Lara Vieira <contato@laravieira.me>
2022-12-08 21:19:53 -0300

    feat(web): Styled Empty to look good.

--- web/src/components/Empty/Empty.component.tsx
+++ web/src/components/Empty/Empty.component.tsx
@@ -4,7 +4,7 @@ function Empty(props: PropsWithChildren) {
   const { children } = props;
 
   return <div className="w-full p-8 pt-40 flex justify-center">
-    <h4>{ children }</h4>
+    <h4 className="text-6 leading-6 font-medium text-caption">{ children }</h4>
   </div>;
 }
 
