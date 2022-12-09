00f0391726b9118929739a7d24be32bc529e9c41
Lara Vieira <contato@laravieira.me>
2022-12-08 21:19:19 -0300

    feat(web): Styled TabHeader as design.

--- web/src/components/TabHeader/TabHeader.component.tsx
+++ web/src/components/TabHeader/TabHeader.component.tsx
@@ -1,5 +1,5 @@
 import { Tab } from '@headlessui/react';
-import { ReactNode } from 'react';
+import { Fragment, ReactNode } from 'react';
 
 type TabHeaderProps = {
   title: string,
@@ -13,27 +13,33 @@ function TabHeader(props: TabHeaderProps) {
   const { title, tabList, children, onChange, selectedIndex } = props;
 
   function renderPanel(child: ReactNode, key: number) {
-    return <Tab.Panel key={key} tabIndex={-1}>
+    return <Tab.Panel key={key}
+                      tabIndex={-1}
+                      className="px-6 box-content">
       { child }
     </Tab.Panel>;
   }
 
   function renderTab(tab:string, key:number) {
-    return <Tab key={key} tabIndex={-1}>
+    return <Tab key={key} as={Fragment}>
       {({selected}) =>
-        <button className={selected ? 'selected' : ''}>
+        <button className={`relative outline-none flex items-end text-3.5 leading-3.5 text-black ${selected ? 'font-bolder pb-[9px] tracking-tighter' : 'font-semibold pb-2 tracking-tight'}`}>
           { tab }
+          <hr className={`${selected ? '' : 'opacity-0'} transition-all absolute bottom-0 translate-y-[70%] shadow-tab shadow-sw-tab w-full h-1 bg-tab border-none rounded-full`}/>
         </button>
       }
     </Tab>;
   }
 
-  return <div>
-    <h2>{ title }</h2>
+  return <div className="mt-[112px] overflow-x-hidden">
     <Tab.Group onChange={onChange} selectedIndex={selectedIndex}>
-      <Tab.List>
-        { tabList.map(renderTab) }
-      </Tab.List>
+      <div className="w-full mx-auto px-6 box-content max-w-[1280px]">
+        <h2 className="text-5 tracking-tight font-semibold leading-5 text-black">{ title }</h2>
+        <Tab.List className="mt-[44px] flex gap-x-[26px] h-9">
+          { tabList.map(renderTab) }
+        </Tab.List>
+      </div>
+      <hr className="w-screen border-none bg-outline h-[1px]"/>
       <Tab.Panels>
         { children.map(renderPanel) }
       </Tab.Panels>
