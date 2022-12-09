c3ae8f049d5043d65859c873a4bc8b6a27ef6b13
Lara Vieira <contato@laravieira.me>
2022-12-08 22:21:44 -0300

    feat(web): Structured ExploreAuctions page.

--- web/src/pages/ExploreAuctions/ExploreAuctions.component.tsx
+++ web/src/pages/ExploreAuctions/ExploreAuctions.component.tsx
@@ -1,31 +1,22 @@
 import PageContainer from '../../components/PageContainer';
-import TabHeader from '../../components/TabHeader';
-import { PlusIcon } from '@heroicons/react/24/outline';
 import Empty from '../../components/Empty';
-import Tabs from '../../components/Tabs';
 import CardSkeleton from '../../components/CardSkeleton';
+import ContentHeader from '../../components/ContentHeader';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
 type MyAuctionsComponentProps = {
-  indexTabHeader: number,
-  onSwitchTabHeader: (index: number) => void,
-  onSwitchTabMyOffers: (index: number) => void,
-  onSwitchTabMyCreated: (index: number) => void,
   loading: boolean,
+  search: string,
+  setSearch: (search: string) => void
   cards: string[],
-  createPanel: boolean
-  showCreatePanel: (show: boolean) => void
 }
 
 function ExploreAuctionsComponent(props: MyAuctionsComponentProps) {
   const {
-    indexTabHeader,
-    onSwitchTabHeader,
-    onSwitchTabMyOffers,
-    onSwitchTabMyCreated,
     loading,
+    search,
+    setSearch,
     cards,
-    createPanel,
-    showCreatePanel
   } = props;
 
   function renderCard(card: string, key: number) {
@@ -38,65 +29,39 @@ function ExploreAuctionsComponent(props: MyAuctionsComponentProps) {
     return <CardSkeleton key={key}/>;
   }
 
-  function renderCards(emptyMessage: string) {
+  function renderCards() {
     if(loading)
       return <div className="flex flex-wrap  gap-4 md:gap-6 w-full box-border">
         { [null, null, null, null].map(renderCardSkeleton) }
       </div>;
+    if(search.length && !cards.length)
+      return <Empty>There no results for that :(</Empty>;
     if(!cards.length)
-      return <Empty>{ emptyMessage }</Empty>;
+      return <Empty>Looks Like no one is doing a auction, you can be one!</Empty>;
     return <div>
       { cards.map(renderCard) }
     </div>;
   }
 
-  function renderCreateAuctionButton() {
-    return <button onClick={() => showCreatePanel(true)}
-                   className="flex gap-2.5 rounded items-center px-4 h-10 bg-button text-3.5 translate-y-[4px] leading-3.5 tracking-tight font-semibold text-white">
-      <PlusIcon className="h-4 w-4 stroke-white opacity-100"/>
-      Create Auction
-    </button>
-  }
-
-  function renderMyOffersPanel() {
-    return <PageContainer>
-      <Tabs tabList={['Auctions live', 'Closed auctions', 'Auctions won']}
-            onChange={onSwitchTabMyOffers}>
-        {[
-          renderCards('The auctions in progress that you birded will appear here'),
-          renderCards('When a auctions your birded close without you winning, they will be listed here'),
-          renderCards('When you win a auction, it will appear here')
-        ]}
-      </Tabs>
-    </PageContainer>;
-  }
-
-  function renderMyCreatedPanel() {
-    return <PageContainer>
-      <Tabs tabList={['Your auctions live', 'Your closed auctions']}
-            rightBar={renderCreateAuctionButton()}
-            onChange={onSwitchTabMyCreated}>
-        {[
-          renderCards('The open auctions you create will be listed here'),
-          renderCards('The closed auctions you created will be listed here.')
-        ]}
-      </Tabs>
-    </PageContainer>;
+  function renderSearchBar() {
+    return <div className="relative flex flex-col mt-[2.5rem]">
+      <input value={search} type="text"
+             onChange={({target: { value }}) => setSearch(value)}
+             placeholder="Search for an auction"
+             className="bg-input rounded-sm placeholder:text-placeholder outline-none p-4 pl-10" />
+      <MagnifyingGlassIcon className="h-6 w-6 absolute inset-0 right-auto"/>
+    </div>;
   }
 
   return <>
-    {/*TODO AUC-5 Add CreateAuction panel
-      * <CreateAuction open={createPanel} onClose={() => showCreatePanel(false)} />*/}
-    <TabHeader
-      title="My Auctions"
-      tabList={['My offers', 'Created auctions']}
-      selectedIndex={indexTabHeader}
-      onChange={onSwitchTabHeader}>
-      {[
-        renderMyOffersPanel(),
-        renderMyCreatedPanel()
-      ]}
-    </TabHeader>
+    <ContentHeader
+      title="Explore auctions"
+      subtitle="Buy and sell NFTs">
+      { renderSearchBar() }
+    </ContentHeader>
+    <PageContainer>
+      { renderCards() }
+    </PageContainer>
   </>;
 }
 
--- web/src/pages/ExploreAuctions/ExploreAuctions.container.tsx
+++ web/src/pages/ExploreAuctions/ExploreAuctions.container.tsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from 'react';
 import ExploreAuctionsComponent from './ExploreAuctions.component';
 
 function ExploreAuctions() {
-  return <></>;
+  const [loading, setLoading] = useState<boolean>(true);
+  const [search, setSearch] = useState<string>('');
+  const [cards, setCards] = useState<string[]>([]);
+
+  useEffect(() => {
+    //TODO filter cards to show the desired ones by the filters/search bar;
+    setCards([]);
+  }, [search]);
+
+  useEffect(() => {
+    //TODO Add all loading variables to this memo
+    setTimeout(() => setLoading(false), 3000);
+  }, []);
+
+  const exploreAuctionsComponentProps = {
+    loading,
+    search,
+    setSearch,
+    cards,
+  }
+
+  return <ExploreAuctionsComponent { ...exploreAuctionsComponentProps }/>;
 }
 
 export default ExploreAuctions;
