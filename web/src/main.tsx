26ad22659ab002e72e810856ecbb7dd61f7ea58e
Alexandre <alexandre01393@gmail.com>
2022-12-12 16:38:51 -0300

    fix(web): add purple shadow in menu mobile and add routes in anchors

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,10 +4,12 @@ import ReactDOM from "react-dom/client";
 import Router from "./router";
 import "@fontsource/inter/variable-full.css";
 import "@fontsource/poppins/700.css";
+import Header from "./components/Header";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <div className="fixed pointer-events-none inset-0 bg-shield backdrop-blur-cover z-[-1]"/>
+    <Header />
     <Router />
   </React.StrictMode>
 );
