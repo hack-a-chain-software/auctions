be803cd15bdb4762aa24b5f263f744bb763f3d16
Alexandre <alexandre01393@gmail.com>
2022-12-12 18:10:21 -0300

    fix(web): fixing box shadow

--- web/src/components/ChooseNFT/ChooseNFT.component.tsx
+++ web/src/components/ChooseNFT/ChooseNFT.component.tsx
@@ -1,5 +1,6 @@
 import { Dialog, RadioGroup } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/solid";
+import { useState } from "react";
 
 type ChooseNFTProps = {
   openNFTSelector: boolean;
@@ -19,30 +20,30 @@ function ChooseNFT({
       onClose={setOpenNFTSelector}
     >
       <Dialog.Backdrop className="fixed inset-0 bg-black/50 z-50" />
-      <Dialog.Panel className="relative bg-white rounded-lg shadow-md overflow-hidden w-[95%] max-w-[620px] h-[443px]">
+      <Dialog.Panel className="relative bg-white rounded-lg shadow shadow-sw overflow-hidden w-[95%] max-w-[620px] h-[443px]">
         <div className="w-full flex justify-between items-center px-9 pr-8 h-12 border-b-[1px] ">
           <Dialog.Title className="text-black w-[76px] mt-1 text-sm font-semibold tracking-tight">
             Select NFTs
           </Dialog.Title>
           <button className="w-[22px] h-[22px] text-title mt-1">
-            <XMarkIcon />
+            <XMarkIcon onClick={() => setOpenNFTSelector(false)} />
           </button>
         </div>
         <RadioGroup
           as="ul"
-          className="grid grid-cols-auto-fit pt-[1.9rem] px-6 justify-items-center gap-1 max-h-[323px] mb-2 overflow-y-auto"
+          className="grid grid-cols-auto-fit pt-[1.9rem] px-6 justify-items-center gap-1 h-[323px] max-h-[323px] mb-2 overflow-y-auto"
         >
-          {[1, 2, 3, 4, 5, 6].map((item) => (
+          {[1, 2, 3].map((item) => (
             <RadioGroup.Option
               as="li"
-              className="relative cursor-pointer outline-none rounded-md hover:bg-space hover:shadow-lg hover:shadow-button/[.25] transition-all outline-none"
+              className="h-[175px] relative cursor-pointer outline-none rounded-md shadow shadow-transparent hover:bg-space hover:shadow-[#474eff40] transition-all outline-none"
               value={item}
               key={item}
             >
               {({ checked }) => (
                 <>
                   <div
-                    className={`p-[.17rem] w-[173px] ${
+                    className={`p-[.17rem] w-[173px] h-[175px] ${
                       checked ? "bg-space" : "bg-transparent"
                     } rounded-md`}
                     // onClick={() => setSelectedNFT()}
@@ -50,7 +51,7 @@ function ChooseNFT({
                     <img
                       src="https://assets-global.website-files.com/5e73a1e3ba24f2cd5dd2232a/62f66985e6fa143898ba6762_Como%20criar%20um%20NFT%20(1)%20(1).jpg"
                       alt=""
-                      className="w-[171px] h-[175px] object-cover rounded-[10px] hover:drop-shadow-xl outline-none"
+                      className="w-[171px] h-[170px] object-cover rounded-[10px] hover:drop-shadow-xl outline-none"
                     />
                   </div>
                   <div className="flex flex-col justify-center absolute backdrop-blur-cover bg-white/80 bottom-3 rounded h-[46px] w-[149px] left-[0.8rem]">
@@ -70,7 +71,7 @@ function ChooseNFT({
           <button className="w-[120px] h-10 border-solid border-outline border-[1px] rounded-md p-2 tracking-tight text-text text-sm font-semibold hover:scale-[102%] transition">
             Cancel
           </button>
-          <button className="w-[112px]  h-[35px] mt-1 bg-button rounded text-highlight tracking-tight text-sm hover:scale-[102%] transition-all">
+          <button className="w-[112px] h-[35px] mt-1 bg-button rounded text-highlight text-sm hover:scale-[102%] transition-all">
             Add to auction
           </button>
         </footer>
