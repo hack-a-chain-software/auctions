65989219b3a37eced1b3eb15115eb9bd85daa5e9
Alexandre <alexandre01393@gmail.com>
2022-12-13 00:53:59 -0300

    fix(web): fixing height card and spacing

--- web/src/components/Card/Card.component.tsx
+++ web/src/components/Card/Card.component.tsx
@@ -11,8 +11,8 @@ function Card({ card }: CardProps) {
   const firstPlace = false;
   const isWon = false;
   const isOwner = false;
-  const offered = true;
-  const outbid = true;
+  const offered = false;
+  const outbid = false;
 
   return (
     <li
@@ -28,8 +28,8 @@ function Card({ card }: CardProps) {
           </span>
         </div>
       )}
-      {firstPlace && offered && (
-        <div className="w-20 h-[19px] md:w-[103px] md:h-[30px] flex justify-center items-center absolute gap-2 bg-space rounded-[30px] z-[1] top-6 right-[1.40rem] md:top-[1.93rem] right-[1.79rem]">
+      {firstPlace && offered && !outbid && (
+        <div className="w-20 h-[19px] md:w-[103px] md:h-[30px] flex justify-center items-center absolute gap-2 bg-space rounded-[30px] z-[1] top-6 right-[1.40rem] md:top-[1.93rem] right-[1.80rem]">
           <span className="text-white justify-around font-medium text-3.5 md:text-sm pr-1 tracking-tight">
             Highest bid
           </span>
@@ -44,12 +44,12 @@ function Card({ card }: CardProps) {
       )}
       <Image
         src="https://nextdrop.s3.amazonaws.com/8287doodles-nft.png"
-        className="w-full h-auto rounded-lg md:w-[280px] md:h-[270px] md:max-w-full object-cover md:rounded-md"
+        className="w-full h-auto  rounded-lg md:w-[280px] md:h-[270px] md:max-w-full object-cover md:rounded-md"
       />
       <div className="flex flex-col w-full ml-3 md:gap-[.8rem] md:ml-0">
         <div className="mt-[2px] md:flex flex-col md:mt-[.7rem]">
           <h3
-            className="text-sm break-keep text-paragraph font-semibold md:text-base tracking-tight"
+            className="text-sm break-keep text-paragraph font-medium md:text-base tracking-tight"
             title="Bored Ape Yatch Club"
           >
             Bored Ape Yatch Club
@@ -63,7 +63,7 @@ function Card({ card }: CardProps) {
             You won!
           </h3>
         ) : (
-          <div className="flex justify-between mt-1 mb-[-4px]">
+          <div className="flex justify-between mt-2 mb-[-4px] md:mt-1">
             <div className="mt-[.2rem]">
               <h3 className="text-sm font-semibold text-paragraph md:text-4 tracking-tight">
                 {isOwner && isClose
@@ -86,7 +86,7 @@ function Card({ card }: CardProps) {
                   : "2d 3h 10m 03s"}
               </span>
             </div>
-            <div className="hidden md:block mt-[.2rem]">
+            <div className="hidden md:block">
               <h3 className="text-paragraph font-semibold text-md text-end tracking-tight">
                 {isOwner && isClose
                   ? "Winner:"
@@ -105,14 +105,14 @@ function Card({ card }: CardProps) {
                   ? "wallet123g454545"
                   : isClose
                   ? "wallet123g454545"
-                  : "0.6 BTH"}
+                  : "0,5 BTH"}
               </span>
             </div>
           </div>
         )}
         {isOwner && (
-          <div className="hidden md:block mb-5">
-            <h3 className="text-black font-semibold text-md tracking-tight">
+          <div className="hidden md:block mb-5 mt-[-2px]">
+            <h3 className="text-paragraph font-semibold text-md tracking-tight">
               Created:
             </h3>
             <span className="text-black font-extrabold text-md tracking-tight">
