f71995b72001f396a3427c443581b432f10a0ca0
Lara Vieira <contato@laravieira.me>
2022-12-16 17:43:05 -0300

    feat(web): Implemented ExploreAuctions hooks.

--- web/src/pages/ExploreAuctions/ExploreAuctions.container.tsx
+++ web/src/pages/ExploreAuctions/ExploreAuctions.container.tsx
@@ -4,19 +4,31 @@ import { useAuctions } from '../../hooks/useAuctions';
 import { Auction } from 'contract_aptos';
 
 function ExploreAuctions() {
+  const { loading: fetchLoading, auctions } = useAuctions();
+
   const [loading, setLoading] = useState<boolean>(true);
   const [search, setSearch] = useState<string>('');
   const [cards, setCards] = useState<Auction[]>([]);
 
   useEffect(() => {
-    //TODO filter cards to show the desired ones by the filters/search bar;
-    setCards([]);
-  }, [search]);
+    if(search === '')
+      return setCards(auctions);
+    setCards(auctions.filter(auction => {
+      if(auction.id.toString().includes(search.toLowerCase()))
+        return true;
+      if(auction.lockedTokenId.token_data_id.creator.toLowerCase().includes(search.toLowerCase()))
+        return true;
+      if(auction.lockedTokenId.token_data_id.collection.toLowerCase().includes(search.toLowerCase()))
+        return true;
+      if(auction.lockedTokenId.token_data_id.name.toLowerCase().includes(search.toLowerCase()))
+        return true;
+      return false;
+    }));
+  }, [search, auctions]);
 
   useEffect(() => {
-    //TODO Add all loading variables to this memo
-    setTimeout(() => setLoading(true), 3000);
-  }, []);
+    setLoading(fetchLoading);
+  }, [fetchLoading]);
 
   const exploreAuctionsComponentProps = {
     loading,
