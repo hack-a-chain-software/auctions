2c03e032d9e327011290b8480d6ac87e835b944b
Lara Vieira <contato@laravieira.me>
2022-12-15 21:45:25 -0300

    feat(web): Created AuctionClient instance.

--- web/src/config/aptosConstants.ts
+++ web/src/config/aptosConstants.ts
@@ -4,6 +4,9 @@ export const MAINNET_NODE_URL = "https://fullnode.mainnet.aptoslabs.com/v1";
 export const LOCAL_FAUCET_URL = "http://127.0.0.1:8081";
 export const DEVNET_FAUCET_URL = "https://faucet.devnet.aptoslabs.com";
 
+export const MODULE_ADDRESS = "0xcd68737b6d647e2f33773d2f9becfb3d573b3205dadb0cf1abccb15256f932ba";
+export const AUCTION_HOUSE_ADDRESS = "0xfe0a4e24a2da836025054f9afc01d4d1151b94762249b7867ee7d35be185410e";
+
 export const NODE_URL = DEVNET_NODE_URL
 export const FAUCET_URL = DEVNET_FAUCET_URL
 
