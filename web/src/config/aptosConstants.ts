9302a44bdfa4f7c0c42eecf6e36f2e419c40ed89
Lara Vieira <contato@laravieira.me>
2022-12-17 11:42:42 -0300

    fix(web): Removed testing configuration.

--- web/src/config/aptosConstants.ts
+++ web/src/config/aptosConstants.ts
@@ -4,9 +4,9 @@ export const MAINNET_NODE_URL = "https://fullnode.mainnet.aptoslabs.com/v1";
 export const LOCAL_FAUCET_URL = "http://127.0.0.1:8081";
 export const DEVNET_FAUCET_URL = "https://faucet.devnet.aptoslabs.com";
 
-export const MODULE_ADDRESS = "0x149afbb68279b766ab2008eb84936036e793c878a78757e7fd39d80055852e21";
-export const AUCTION_HOUSE_ADDRESS = "0xd8592bdd667c66fca653042ac1b43f00bfdbaf0d44350c2ae068a151ee7d09fb";
+export const NODE_URL = DEVNET_NODE_URL
+export const FAUCET_URL = DEVNET_FAUCET_URL
 
-export const NODE_URL = LOCAL_NODE_URL;
-export const FAUCET_URL = LOCAL_FAUCET_URL;
-
+export const MODULE_ADDRESS = "0x76ac4f4609bf701f2c953209cb7422956e8defce1e449213e9829fb0e38143ad";
+export const AUCTION_HOUSE_ADDRESS = "0x00158c3229dd80bad850ca24ecdf737f22a5f06d138f47c26079493d73a4e184"
+export const OWNER_PRIVATE_KEY = "0x6d39665992f7e4eb3abcdac8d83d8e5a007b033c926a3d2b1935f6464b1d8ef4"
