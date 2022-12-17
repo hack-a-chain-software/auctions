e7f31383e3ae011464fea4dddfd3e51b5d93af77
Alexandre <alexandre01393@gmail.com>
2022-12-16 23:56:01 -0300

    fix(web): change makeOffer function

--- web/src/hooks/useMakeOffer.tsx
+++ web/src/hooks/useMakeOffer.tsx
@@ -2,16 +2,17 @@ import { useWallet } from "@manahippo/aptos-wallet-adapter";
 import { useCallback } from "react";
 import { AuctionClient } from "../config/aptosClient";
 
-export const useMakeOffer = (id: string, bid: string, coinType: string) => {
+export const useMakeOffer = (id: string, bid: string |   null, coinType: string) => {
   const { signAndSubmitTransaction } = useWallet();
 
   const makeOffer = useCallback(async () => {
-    await AuctionClient.bid(
-      (payload) => signAndSubmitTransaction(payload),
-      id,
-      bid,
-      coinType
-    );
+    console.log(bid)
+    // await AuctionClient.bid(
+    //   (payload) => signAndSubmitTransaction(payload),
+    //   id,
+    //   bid,
+    //   coinType
+    // );
   }, []);
 
   return { makeOffer };
