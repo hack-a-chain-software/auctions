4631a068bf141b06f9a4eda4d404a4a8211ecbca
Lara Vieira <contato@laravieira.me>
2022-12-19 19:47:43 -0300

    fix(web): Refactor Auction Card.

--- web/src/config/aptosConstants.ts
+++ web/src/config/aptosConstants.ts
@@ -4,9 +4,9 @@ export const MAINNET_NODE_URL = "https://fullnode.mainnet.aptoslabs.com/v1";
 export const LOCAL_FAUCET_URL = "http://127.0.0.1:8081";
 export const DEVNET_FAUCET_URL = "https://faucet.devnet.aptoslabs.com";
 
-export const MODULE_ADDRESS = "0xcd68737b6d647e2f33773d2f9becfb3d573b3205dadb0cf1abccb15256f932ba";
-export const AUCTION_HOUSE_ADDRESS = "0xfe0a4e24a2da836025054f9afc01d4d1151b94762249b7867ee7d35be185410e";
+export const MODULE_ADDRESS = "0x149afbb68279b766ab2008eb84936036e793c878a78757e7fd39d80055852e21";
+export const AUCTION_HOUSE_ADDRESS = "0xd8592bdd667c66fca653042ac1b43f00bfdbaf0d44350c2ae068a151ee7d09fb";
 
-export const NODE_URL = DEVNET_NODE_URL
-export const FAUCET_URL = DEVNET_FAUCET_URL
+export const NODE_URL = LOCAL_NODE_URL;
+export const FAUCET_URL = LOCAL_FAUCET_URL;
 
