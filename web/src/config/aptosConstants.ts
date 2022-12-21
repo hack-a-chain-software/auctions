f7c027f5fd477628f54cc42d6250828b1abf96c8
antonio <amrh07@gmail.com>
2022-12-21 19:36:35 -0300

    finished eval transaction resut func

--- web/src/config/aptosConstants.ts
+++ web/src/config/aptosConstants.ts
@@ -1,15 +1,15 @@
-export const LOCAL_NODE_URL = "http://127.0.0.1:5173/"
-export const DEVNET_NODE_URL = "https://fullnode.devnet.aptoslabs.com/v1"
-export const MAINNET_NODE_URL = "https://fullnode.mainnet.aptoslabs.com/v1"
-export const LOCAL_FAUCET_URL = "http://127.0.0.1:5173/"
-export const DEVNET_FAUCET_URL = "https://faucet.devnet.aptoslabs.com"
+export const LOCAL_NODE_URL = "http://127.0.0.1:5173/";
+export const DEVNET_NODE_URL = "https://fullnode.devnet.aptoslabs.com/v1";
+export const MAINNET_NODE_URL = "https://fullnode.mainnet.aptoslabs.com/v1";
+export const LOCAL_FAUCET_URL = "http://127.0.0.1:5173/";
+export const DEVNET_FAUCET_URL = "https://faucet.devnet.aptoslabs.com";
 
-export const NODE_URL = LOCAL_NODE_URL
-export const FAUCET_URL = LOCAL_FAUCET_URL
+export const NODE_URL = DEVNET_NODE_URL;
+export const FAUCET_URL = DEVNET_FAUCET_URL;
 
 export const MODULE_ADDRESS =
-	"0xb0aa7a971dc3d701bb029e3d773829abcc9d944b6f6f5778f935b616ae02a69b"
+  "0x6ae7b29b45a1b95cbf4dfde813533bfa87dfc41bed186d6b72c9f798ea1860c6";
 export const AUCTION_HOUSE_ADDRESS =
-	"0x71cefa14d0aff8e334fe7be3a5228783f26c971fb574063fa31c155ea18bed1e"
+  "0x0ad356ad526cf90b1ad1f4928816fa9f3bd05431e0cbd979c14a27960f91a27d";
 export const OWNER_PRIVATE_KEY =
-	"0x6c6a80da5cf5041a7f89e37177d0fb6dce8b85027e62ec297d0f876905c1bd81"
+  "0x8a1af3b69e975c37fd8a4db2b4af16762046ef6378300f667243a60935bd3f1d";
