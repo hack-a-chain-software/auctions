34303417f75eeb70e2661e42988b32fb90023c89
Jasso <veigajoaoa@gmail.com>
2022-12-21 20:12:10 -0300

    feat(contracts, web): add testnet seed script and generalize front to use any contract based on env

--- web/src/config/aptosClient.ts
+++ web/src/config/aptosClient.ts
@@ -1,8 +1,7 @@
-import { FaucetClient, AptosClient, CoinClient, TokenClient } from "aptos"
+import { AptosClient, CoinClient, TokenClient } from "aptos"
 import { AuctionHouseClient } from "contract_aptos"
-import { NODE_URL, FAUCET_URL, MODULE_ADDRESS, AUCTION_HOUSE_ADDRESS, GRAPHQL_URL } from "./aptosConstants"
+import { NODE_URL, MODULE_ADDRESS, AUCTION_HOUSE_ADDRESS, GRAPHQL_URL } from "./aptosConstants"
 
-export const faucetClient = new FaucetClient(NODE_URL, FAUCET_URL)
 export const aptosClient = new AptosClient(NODE_URL)
 export const coinClient = new CoinClient(aptosClient)
 export const AuctionClient = new AuctionHouseClient(NODE_URL, GRAPHQL_URL, MODULE_ADDRESS, AUCTION_HOUSE_ADDRESS);
--- web/src/config/aptosConstants.ts
+++ web/src/config/aptosConstants.ts
@@ -1,16 +1,41 @@
-export const LOCAL_NODE_URL = "http://127.0.0.1:8080";
-export const DEVNET_NODE_URL = "https://fullnode.devnet.aptoslabs.com/v1";
-export const MAINNET_NODE_URL = "https://fullnode.mainnet.aptoslabs.com/v1";
-export const LOCAL_FAUCET_URL = "http://127.0.0.1:8081";
-export const DEVNET_FAUCET_URL = "https://faucet.devnet.aptoslabs.com";
+import { useEnv } from "../hooks/useEnv";
 
-export const TESTNET_GRAPHQL_URL = "";
-export const MAINNET_GRAPHQL_URL = "";
-export const OTHER_NETS_GRAPHQL_URL = null;
+const auctionNetwork = useEnv("AUCTION_NETWORK");
 
-export const NODE_URL = LOCAL_NODE_URL
-export const FAUCET_URL = LOCAL_FAUCET_URL
-export const GRAPHQL_URL = OTHER_NETS_GRAPHQL_URL;
+const LOCAL_NODE_URL = "http://127.0.0.1:8080";
+const DEVNET_NODE_URL = "https://fullnode.devnet.aptoslabs.com/v1";
+const TESTNET_NODE_URL = "https://fullnode.testnet.aptoslabs.com/v1";
+const MAINNET_NODE_URL = "https://fullnode.mainnet.aptoslabs.com/v1";
 
-export const MODULE_ADDRESS = "0xaf400dd991f52ea9e587ad905e679711c7c8197fdba592519773ffb5515f27ed";
-export const AUCTION_HOUSE_ADDRESS = "0x41c9d685694f3b349e0a6ed6c0a3311b4002b0cf6d4553e9811f862daed49c9c"
+const TESTNET_GRAPHQL_URL = "https://indexer-testnet.staging.gcp.aptosdev.com/v1/graphql";
+const MAINNET_GRAPHQL_URL = "https://indexer.mainnet.aptoslabs.com/v1/graphql";
+const OTHER_NETS_GRAPHQL_URL = null;
+
+export const MODULE_ADDRESS = useEnv("AUCTION_MODULE_ADDRESS");
+export const AUCTION_HOUSE_ADDRESS = useEnv("AUCTION_INSTANCE_ADDRESS")
+
+
+
+let nodeUrl;
+let graphqlUrl;
+if (auctionNetwork === "mainnet") {
+    nodeUrl = MAINNET_NODE_URL;
+    graphqlUrl = MAINNET_GRAPHQL_URL;
+} else if (auctionNetwork === "testnet") {
+    nodeUrl = TESTNET_NODE_URL;
+    graphqlUrl = TESTNET_GRAPHQL_URL;
+} else if (auctionNetwork === "devnet") {
+    nodeUrl = DEVNET_NODE_URL;
+    graphqlUrl = OTHER_NETS_GRAPHQL_URL;
+} else if (auctionNetwork === "localnet") {
+    nodeUrl = LOCAL_NODE_URL;
+    graphqlUrl = OTHER_NETS_GRAPHQL_URL;
+} else {
+    throw new Error(`AUCTION_NETWORK must be mainnet", "testnet", "devnet" or "localnet", found ${auctionNetwork}`)
+}
+
+export const NODE_URL = nodeUrl
+export const GRAPHQL_URL = graphqlUrl;
+
+
+
