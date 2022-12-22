c3ddd46ee0d86415461559a9126d5647abb61d99
Jasso <veigajoaoa@gmail.com>
2022-12-22 16:02:22 -0300

    test remove stuff

--- web/api/auction/[id].ts
+++ web/api/auction/[id].ts
@@ -6,10 +6,10 @@ import { NODE_URL, MODULE_ADDRESS, AUCTION_HOUSE_ADDRESS, GRAPHQL_URL } from "..
 export default async function handler(request: VercelRequest, response: VercelResponse) {
     response.setHeader('Cache-Control', 's-maxage=300, stale-while-revalidate');
     const { id } = request.query;
-    const client = new contractAptos.AuctionHouseClient(NODE_URL, GRAPHQL_URL, MODULE_ADDRESS, AUCTION_HOUSE_ADDRESS);
-    const auction = await client.getAllAuctions(Number(id), 1);
+    // const client = new contractAptos.AuctionHouseClient(NODE_URL, GRAPHQL_URL, MODULE_ADDRESS, AUCTION_HOUSE_ADDRESS);
+    // const auction = await client.getAllAuctions(Number(id), 1);
     response.status(200).json({ 
-        auction,
+        // auction,
         id,
         NODE_URL,
         MODULE_ADDRESS,
