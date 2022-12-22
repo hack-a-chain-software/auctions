7151390d8d2a1cdf03caf3075c1cb64c17f7526f
Jasso <veigajoaoa@gmail.com>
2022-12-22 14:54:33 -0300

    remove deps

--- web/api/auction/[id].ts
+++ web/api/auction/[id].ts
@@ -1,9 +1,9 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { AuctionClient } from '../setup/env';
+// import { AuctionClient } from '../setup/env';
 
 export default async function handler(request: VercelRequest, response: VercelResponse) {
     response.setHeader('Cache-Control', 's-maxage=300, stale-while-revalidate');
     const { id } = request.query;
-    const auction = await AuctionClient.getAllAuctions(Number(id), 1);
-    response.status(200).json({ auction });
+    // const auction = await AuctionClient.getAllAuctions(Number(id), 1);
+    response.status(200).json({ auction: id });
 }
