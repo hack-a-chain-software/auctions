1551e532115b16f3d8ed1267ed64c0fa8ef1d9d1
Jasso <veigajoaoa@gmail.com>
2022-12-22 14:59:19 -0300

    test decoupling

--- web/api/auction/[id].ts
+++ web/api/auction/[id].ts
@@ -1,9 +1,19 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 // import { AuctionClient } from '../setup/env';
+import { AuctionHouseClient } from "contract_aptos"
+import { NODE_URL, MODULE_ADDRESS, AUCTION_HOUSE_ADDRESS, GRAPHQL_URL } from "../setup/constants"
 
 export default async function handler(request: VercelRequest, response: VercelResponse) {
     response.setHeader('Cache-Control', 's-maxage=300, stale-while-revalidate');
     const { id } = request.query;
-    // const auction = await AuctionClient.getAllAuctions(Number(id), 1);
-    response.status(200).json({ auction: id });
+    const client = new AuctionHouseClient(NODE_URL, GRAPHQL_URL, MODULE_ADDRESS, AUCTION_HOUSE_ADDRESS);
+    const auction = await client.getAllAuctions(Number(id), 1);
+    response.status(200).json({ 
+        auction,
+        id,
+        NODE_URL,
+        MODULE_ADDRESS,
+        AUCTION_HOUSE_ADDRESS,
+        GRAPHQL_URL,
+    });
 }
