281d4f093d56390dcad34ca176074e90f0c06ebc
Jasso <veigajoaoa@gmail.com>
2022-12-22 11:13:39 -0300

    add id

--- web/api/auction/[id].ts
+++ web/api/auction/[id].ts
@@ -1,9 +1,9 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { AuctionClient } from './env';
+import { AuctionClient } from '../env';
 
 export default async function handler(request: VercelRequest, response: VercelResponse) {
     response.setHeader('Cache-Control', 's-maxage=300');
-    const id = request.body.id;
-    const auction = await AuctionClient.getAllAuctions(id, 1);
+    const { id } = request.query;
+    const auction = await AuctionClient.getAllAuctions(Number(id), 1);
     response.status(200).json({ auction });
 }
