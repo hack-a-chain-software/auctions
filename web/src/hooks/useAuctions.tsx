c445124c4b574fdbd322e04d9b25493211685679
Lara Vieira <contato@laravieira.me>
2022-12-21 21:48:49 -0300

    fix(web): Change fetching implementation.

--- web/src/hooks/useAuctions.tsx
+++ web/src/hooks/useAuctions.tsx
@@ -1,26 +1,33 @@
 import { Auction } from 'contract_aptos';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { AuctionClient } from '../config/aptosClient';
+import { message } from 'antd';
 
 export const useAuctions: () => {
   auctions: Auction[]
+  fetch: () => void,
   loading: boolean,
 } = () => {
   const [loading, setLoading] = useState<boolean>(true);
   const [auctions, setAuctions] = useState<Auction[]>([]);
   let running = false;
 
-  useEffect(() => {
+  function fetch() {
     if(running)
       return;
 
-    running = true;
+    setLoading(running = true);
     AuctionClient.getAllAuctionsLen()
       .then(amount => AuctionClient.getAllAuctions(0, amount))
       .then(setAuctions)
       .then(() => running = false)
-    return () => setLoading(false);
-  }, []);
+      .then(setLoading)
+      .catch(error => {
+        console.error(error);
+        message.error("Unable to fetch the auctions.").then();
+        setLoading(running = false);
+      });
+  }
 
-  return { loading, auctions };
+  return { loading, auctions, fetch };
 }
