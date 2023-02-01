8513beddfaa5d065ed5038c77230881a49ae93c9
Alexandre <alexandre01393@gmail.com>
2023-02-01 08:52:02 -0300

    fix(web): remove tailwind scroll bar lib

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -117,7 +117,7 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwind-scrollbar-hide"), require("tailwind-scrollbar")],
+  plugins: [require("tailwind-scrollbar-hide")],
 };
 
 function hexToRgb(hex) {
